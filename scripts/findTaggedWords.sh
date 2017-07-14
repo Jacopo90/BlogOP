f4cc7b354e2715cb1a41950881d5e56f85747e4d
Jacopo Pappalettera <Jacopo@iMac-di-Jacopo.homenet.telecomitalia.it>
2017-07-14 22:19:31 +0200

    working on

--- scripts/findTaggedWords.sh
+++ scripts/findTaggedWords.sh
@@ -4,26 +4,75 @@ selected_tag=$1
 tags_path=$2
 source_path=$3
 
+
+## check if tah path is valid
+if [ ! -f "$tags_path" ];then
+    echo "ERROR: tags path is not valid"
+    exit 1
+fi
+
 all_keys=$(jq 'keys|.[]' $tags_path)
 
 search_tag=""
 for key in $all_keys
 do
+
   clean_key=$(echo $key | tr -d '"')
   object=$(jq .''$key'' $tags_path)
   clean_object=$(echo $object | tr -d '"')
   if [ $clean_key = $selected_tag ]
   then
-   search_tag="$clean_object" 
+    search_tag="$clean_object"
   fi
 done
 
-results=$(grep -wo $search_tag'\w*' $source_path/*)
+if [ -z "$search_tag" ];then
+    echo "ERROR: no tag found"
+    exit 1
+fi
 
-final_list=[]
+##removing all occurrences of every single tag
+for key in $all_keys
+do
+    clean_key=$(echo $key | tr -d '"')
+    if [  $clean_key = $selected_tag ]; then
+        echo "continue" $key $selected_tag
+        continue
+    fi
+    clean_key=$(echo $key | tr -d '"')
+    object=$(jq .''$key'' $tags_path)
+    clean_object=$(echo $object | tr -d '"')
+    echo "removing all occurrences of" $clean_object
+
+    all_text=$(cat $source_path/* |  sed "s/$clean_object//g")
+
+    echo $all_text
+done
+exit 1
+
+results=$(grep -oh $search_tag'.\w*' $source_path/*)
+
+if [ -z "$results" ]; then
+    echo "ERROR: results are empty"
+    exit 1
+fi
+
 for occurrence in $results
 do
- word=$(echo $occurrence | sed "s@.*$search_tag@@g")
+    word=$occurrence
+    for key in $all_keys
+    do
+    clean_key=$(echo $key | tr -d '"')
+    object=$(jq .''$key'' $tags_path)
+    clean_object=$(echo $object | tr -d '"')
+echo "deleting .." $clean_object "from" $word
+
+    word=$(echo $word | sed "s@clean_object@@g")
+echo $word
+    done
+
+
+#echo $word
 done
 
-echo $final_list
+exit 0
