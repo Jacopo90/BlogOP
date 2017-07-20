16a1990db4483d1f55b250bb6588c9572464e454
Jacopo Pappalettera <blackbart90@gmail.com>
2017-07-20 16:32:57 +0200

    fix find.sh

--- scripts/find.sh
+++ scripts/find.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 ## author : Jacopo Pappalettera
-## edited : 19/07/2017
+## edited : 20/07/2017
 ## Takes every values from a json file with a specified tag name! and pass it to the find.sh script
 ## Is this script can be useful???
 
@@ -10,6 +10,16 @@ source_path=$texts_path
 key_list=$1
 json_file=$2
 
+if [[ ! $key_list ]]; then
+	echo "ERROR: no key list provided"
+	exit 1
+fi
+
+if [[ ! $json_file ]]; then
+	echo "ERROR: no json file provided"
+	exit 1
+fi
+
 list=$(jq .''$key_list'|.[]' $json_file)
 for name in $list
 do
@@ -18,4 +28,4 @@ do
 		sh find_word.sh $clean_name $source_path
 	fi
 done
-
+exit 0
